dc0dbf674f95a212cdc7f6b248a04968e6134475
Dogkaebi5 <st9dog@gmail.com>
2023-02-15 00:38:15 +0900

    Serverless Func 설정

--- src/store/movie.js
+++ src/store/movie.js
@@ -20,7 +20,13 @@ export const searchMovies = async page => {
   }
 
   try {
-    const res = await fetch(`https://omdbapi.com/?&apikey=3b7469b3&s=${store.state.searchText}&page=${page}`);
+    const res = await fetch('/api/movie', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: store.state.searchText,
+        page
+      })
+    })
     // const json = await res.json();
     const { Search, totalResults, Response, Error } = await res.json();
     if (Response === "True") {
@@ -42,7 +48,12 @@ export const searchMovies = async page => {
 
 export const getMovieDetails = async id => {
   try {
-    const res = await fetch(`https://omdbapi.com/?&apikey=3b7469b3&i=${id}&plot=full`)
+    const res = await fetch('/api/movie', {
+      method: 'POST',
+      body: JSON.stringify({
+        id
+      })
+    })
     store.state.movie = await res.json()
   } catch (error) {
     console.log(error)
